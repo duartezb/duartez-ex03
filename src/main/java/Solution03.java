edb6463a98207456ef288a383360e4013ac9d9a2
Billy Duartez <duartezb@knights.ucf.edu>
2021-09-05 14:11:00 -0400

    .

---
+++ src/main/java/Solution03.java
@@ -0,0 +1,28 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Billy Duartez
+ */
+
+/*Program prompts for a quote and an author then displays the quotation
+and author back to user*/
+
+import java.util.Scanner;
+
+public class Solution03 {
+    public static void main(String[] args) {
+
+        //intro question
+        System.out.println("What is the quote? ");
+        Scanner input = new Scanner(System.in);
+        String quote = input.nextLine();
+
+        //ask second question
+        System.out.println("Who said it? ");
+        String author = input.nextLine();
+
+        //repeat back to user
+        System.out.println(author + " says, \""+ quote+"\"");
+
+
+    }
+}
